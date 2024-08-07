c4a1039b3609d8d81452bf46fab20441817d5b67
Abun <85024172+AhBun9@users.noreply.github.com>
2024-08-07 15:08:56 +0800

    Add files via upload

---
+++ babel.config.js
@@ -0,0 +1,53 @@
+/*
+ * This file is part of OrangeHRM
+ *
+ * Copyright (C) 2020 onwards OrangeHRM (https://www.orangehrm.com/)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          screens: './src/screens',
+          components: './src/components',
+          images: './src/images',
+          store: './src/store',
+          layouts: './src/layouts',
+          theme: './src/theme',
+          lib: './src/lib',
+          services: './src/services',
+        },
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
+};
